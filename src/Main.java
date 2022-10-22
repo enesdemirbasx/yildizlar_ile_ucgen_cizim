cd52844ed87fe0dfca722bb58e9a673fd0d3ee06
enesdemirbasx <104502971+enesdemirbasx@users.noreply.github.com>
2022-10-23 02:55:13 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp=new Scanner(System.in);
-        System.out.print("Üçgen boyutu için bir sayi giriniz: ");
+        System.out.print("Üçgen boyutu için bir çift sayi giriniz: ");
         int sayi= inp.nextInt();
+        if (sayi%2==1){
+            System.out.println("Tek sayı girdiniz. Lütfen çift sayı giriniz!!!");
+        }
         int a=1;
+        int say2=sayi/2;
+        int stop=say2;
+        int c=sayi;
         for (int i=0;i<sayi+1;i++){
             for (int j=0;j<sayi;j++){
                 System.out.print(" ");
@@ -15,9 +21,20 @@ public class Main {
             sayi=sayi-1;
             a=a+2;
             System.out.println();
-
-
         }
+        for (int b=0;b<stop;b++){
+            for (int n=0;n<say2+1;n++){
+                System.out.print(" ");
+            }
+            for (int m=0;m<c-1;m++){
+                System.out.print("*");
+            }
+            say2=say2+1;
+            c=c-2;
+            System.out.println();
+        }
+
+
 
     }
 }
